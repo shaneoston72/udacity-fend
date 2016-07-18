447d081ed307dd8a498608b7b068be5c64ea7857
Shane Oston Stowe <shaneoston@shaneoston.com>
2016-07-18 13:38:17 +0100

    Completed project minimum requirements and added scoring and number of lives to game

--- p5/README.md
+++ p5/README.md
@@ -1,6 +1,10 @@
-frontend-nanodegree-arcade-game
-===============================
+# FEND Arcade game
+This is a simple game written in Javascript that runs in the browser. The rules are simple:
+* Move your player around the screen using the arrow keys on the keyboard.
+* Avoid contact with those nasty bugs. Colliding with one will reset the game and you lose a life.
+* Reach the water to get a point.
 
-Students should use this [rubric](https://review.udacity.com/#!/projects/2696458597/rubric) for self-checking their submission. Make sure the functions you write are **object-oriented** - either class functions (like Player and Enemy) or class prototype functions such as Enemy.prototype.checkCollisions, and that the keyword 'this' is used appropriately within your class and class prototype functions to refer to the object the function is called upon. Also be sure that the **readme.md** file is updated with your instructions on both how to 1. Run and 2. Play your arcade game.
-
-For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
+## Game installation
+* Download or clone the repository.  
+* Open index.html in the browser.
+* Play!
--- p5/js/app.js
+++ p5/js/app.js
@@ -1,36 +1,121 @@
-// Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+/* Creates a random type enemy to which speed can be assigned.
+ * Given two values, min & max, generates a number between the
+ * two. Used example from developer.mozilla.org (Math.random()).
+ */
+var random = function(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+};
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+// Heart for lives count
+var heart = '<img src="images/Heart.png">';
+
+// Enemy class and prototype functions
+var Enemy = function() {
+  // Generates a random number to assign to bug as a type
+  this.type = random(1,5);
+  // Specifies the image to use for the bug
+  this.sprite = 'images/enemy-bug.png';
+  // Sets the position of the bug on the x-axis
+  this.x = this.type *  -101;
+  // Sets the position of the bug on the y-axis
+  this.y = random(0, 3) * 83 + 54;
+  // Sets a random speed at which the bug moves across the board
+  this.speed = this.type * 100;
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+  // You should multiply any movement by the dt parameter
+  // which will ensure the game runs at the same speed for
+  // all computers.
+  this.x += this.speed * dt;
+
+  /* Removes the bug when it reaches the right edge of the
+  * board and adds another.
+  */
+  for (var enemy = 0; enemy < allEnemies.length; enemy++) {
+    if (allEnemies[enemy].x > 505) {
+      allEnemies.splice(enemy, 1, new Enemy());
+    }
+  }
 };
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+
+// Player class and prototype functions
+var Player = function() {
+  this.sprite = 'images/char-boy.png';
+  this.x = random(0, 4) * 101;
+  this.y = 404;
+  this.lives = [heart, heart, heart];
+  this.score = 0;
+  document.getElementsByClassName('lives')[0].innerHTML = 'Lives: ' + this.lives.join(' ');
+  document.getElementsByClassName('score')[0].innerHTML = 'Score: ' + this.score;
+};
+
+Player.prototype.update = function() {
+  // Checks for collisions and, upon collision, resets game
+  for (var enemy = 0; enemy < allEnemies.length; enemy++) {
+    if (Math.abs(allEnemies[enemy].x - this.x) < 50 &&
+        Math.abs(allEnemies[enemy].y - this.y) < 66) {
+          this.lives.pop();
+          // checks if any lives left; if not, game over; if yes, resets
+          if (this.lives.length === 0) {
+            this.gameOver();
+          } else {
+            this.reset();
+          }
+    }
+  } // scores a point if the player reaches the water
+  if (this.y <= 0) {
+    this.score += 1;
+    this.reset();
+  }
+};
+
+// Ends the game and resets it to its start
+Player.prototype.gameOver = function() {
+  var gameOver = alert('Game Over!  Press OK to play again.');
+  player = new Player();
+};
+
+// Resets player to bottom row
+Player.prototype.reset = function() {
+  document.getElementsByClassName('score')[0].innerHTML = 'Score: ' + this.score;
+  document.getElementsByClassName('lives')[0].innerHTML = 'Lives: ' + this.lives.join(' ');
+  this.x = random(0,4) * 101;
+  this.y = 402;
+}
+
+// Draws the player on the screen
+Player.prototype.render = function() {
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// Moves a Player instance according to keyboard input
+Player.prototype.handleInput = function(keycode) {
+  if (keycode === 'up' && this.y > 44) {
+    this.y -= 83;
+  } else if (keycode === 'left' && this.x > 83) {
+    this.x -= 101;
+  } else if (keycode === 'right' && this.x < 404) {
+    this.x += 101;
+  } else if (keycode === 'down' && this.y < 332) {
+    this.y += 83;
+  }
+};
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
-
+var allEnemies = [new Enemy(), new Enemy(), new Enemy()];
+var player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
