b6b0af6d0c33ff1463ba6aeae48ff055832a43e8
Shane Stowe <shane.stowe@printed.com>
2016-11-05 20:09:22 +0000

    added first markers from  array on map

---
+++ p7/src/js/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// Map locations
+var churches = [
+    {
+        name: 'Knap of Howar',
+        position: { lat: 59.34935, lng: -2.91085 }
+    },
+    {
+        name: 'West Kennet Long Barrow',
+        position: { lat: 51.4086025, lng: -1.8503273 }
+    }
+]
--- p7/src/js/mapsApi.js
+++ p7/src/js/mapsApi.js
@@ -1,8 +1,24 @@
+'use strict';
+
 var map;
+var markers = [];
 
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 51.513678, lng: -0.098383},
+    center: { lat: 51.513678, lng: -0.098383 },
     zoom: 13
   });
+
+  var bounds = new google.maps.LatLngBounds();
+
+  for (var i = 0; i < churches.length; i++) {
+      var position = churches[i].position;
+
+      var marker = new google.maps.Marker({
+          map: map,
+          position: position,
+      });
+
+      markers.push(marker);
+  }
 }
