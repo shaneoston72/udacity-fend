77fabafb350f5cd4f1967c9ae9df48a0be5ec636
Shane Stowe <shane.stowe@printed.com>
2016-11-10 10:46:36 +0000

    completed p8 project

---
+++ p8/README.md
@@ -0,0 +1,9 @@
+# Project Overview
+
+This is the final project of Udacity's Front End Web Developer Nanodegree.  It is meant to show familiarity with Test-Driven Development and Unit Testing using Jasmine.
+
+## Instructions to use
+Download or clone the repository.  Run index.html through a local web server or open the file directly.  You see the tests response at the bottom of the app.
+
+## Tests
+Tests are found in ```jasmine/spec/```.  Feel free to change tests to see them fail or add additional tests.  If you'd like to add a test to the repository, create a pull request. 
---
+++ p8/jasmine/spec/feedreader.js
@@ -0,0 +1,92 @@
+/* feedreader.js
+ *
+ * This is the spec file that Jasmine will read and contains
+ * all of the tests that will be run against your application.
+ */
+
+/* We're placing all of our tests within the $() function,
+ * since some of these tests may require DOM elements. We want
+ * to ensure they don't run until the DOM is ready.
+ */
+$(function() {
+    /* This is our first test suite - a test suite just contains
+    * a related set of tests. This suite is all about the RSS
+    * feeds definitions, the allFeeds variable in our application.
+    */
+    describe('RSS Feeds', function() {
+        /* This is our first test - it tests to make sure that the
+         * allFeeds variable has been defined and that it is not
+         * empty. Experiment with this before you get started on
+         * the rest of this project. What happens when you change
+         * allFeeds in app.js to be an empty array and refresh the
+         * page?
+         */
+        it('are defined', function() {
+            expect(allFeeds).toBeDefined();
+            expect(allFeeds.length).not.toBe(0);
+        });
+
+        it('URL is defined', function() {
+            allFeeds.forEach(function(feed) {
+                expect(feed.url).toBeDefined();
+                expect(feed.url).not.toBe('');
+            });
+         });
+
+         it('name is defined', function() {
+            allFeeds.forEach(function(feed) {
+                expect(feed.name).toBeDefined();
+                expect(feed.name).not.toBe('');
+            });
+         });
+    });
+
+    describe('The menu', function () {
+
+        it('should be hidden by default', function () {
+            expect($('body').hasClass('menu-hidden')).toBe(true);
+        });
+
+        it('changes visibility when clicked', function () {
+            $('a.menu-icon-link').click();
+            expect($('body').hasClass('menu-hidden')).toBe(false);
+            $('a.menu-icon-link').click();
+            expect($('body').hasClass('menu-hidden')).toBe(true);
+        });
+    });
+
+    describe('Initial Entries', function () {
+        beforeEach(function (done) {
+            loadFeed(0);
+            done();
+        });
+
+        it('has a single entry after loadFeed runs', function () {
+            expect($('.entry')).not.toBe(null);
+        });
+    });
+
+    describe('New Feed Selection', function () {
+        var feed0,
+            feed1;
+
+        beforeEach(function (done) {
+            loadFeed(0, done);
+        });
+
+        it('content changes when a new feed is loaded', function (done) {
+            feed0 = $('feed').html();
+            loadFeed(1);
+
+            expect($('.feed').html()).not.toEqual(feed0);
+            done();
+        });
+    });
+
+    describe('Check that app can run', function () {
+        it('that allFeeds exists and has data', function() {
+            expect(allFeeds).toBeDefined();
+            expect(allFeeds.length).not.toBe(0);
+        });
+    });
+}());
